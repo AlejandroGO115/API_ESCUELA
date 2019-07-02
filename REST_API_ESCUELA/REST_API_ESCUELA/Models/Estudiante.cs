4ef29dde57806a8c388d2a99081736f987c6363d
AlejandroGO115 <agarciiaolivoo@gmail.com>
2019-07-02 17:25:28 -0500

    Creación

---
+++ REST_API_ESCUELA/REST_API_ESCUELA/Models/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST_API_ESCUELA.Models
+{
+    public class Estudiante
+    {
+        public int IdAlumnos { get; set; }
+        public string Nombre { get; set; }
+        public string Edad { get; set; }
+    }
+}
