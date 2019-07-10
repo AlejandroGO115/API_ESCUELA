058abe555ecdac52991d78d28c01da91211eb131
AlejandroGO115 <agarciiaolivoo@gmail.com>
2019-07-10 17:05:36 -0500

    KHA

---
+++ REST_API_ESCUELA/REST_API_ESCUELA/Controllers/MateriaController.cs
@@ -0,0 +1,38 @@
+﻿using REST_API_ESCUELA.ADO;
+using REST_API_ESCUELA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace REST_API_ESCUELA.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/Materia")]
+    public class MateriaController : ApiController
+    {
+        [HttpGet]
+        [Route("GetAll")]
+        public IHttpActionResult GetMaterias()
+        {
+            return Ok(ADO_Materias.Obtener_Todo());
+        }
+
+        [HttpPost]
+        [Route("Insert")]
+        public IHttpActionResult InsertMateria(Materia m)
+        {
+            if (m == null)
+                return BadRequest("Error");
+
+            Materia ma = ADO_Materias.Insertar(m);
+
+            if (ma != null)
+                return Ok(ma);
+            else
+                return BadRequest("Error");
+        }
+    }
+}
